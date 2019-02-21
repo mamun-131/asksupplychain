3d29c808ba83a00c602733d4567fb0728a2b2732
mamun_131 <mamun_131@hotmail.com>
2019-02-20 22:04:41 -0500

    new

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def mrpcsearch(mat1):
 	df = pd.read_csv("mrpcdata.csv")
 	#dataA = df.loc[df['Material'] == mat1, 'MRPC']	
 	dataA = df.loc[df['Material'] == mat1]	
-	out = dataA.to_json(orient='records')[1:-1]
+	out = dataA.to_json(orient='records')
 	#out = df .to_json(orient='records')[1:-1].replace('},{', '} {')
 	#out = dataA.to_json(orient='records')
 	return out
@@ -34,7 +34,7 @@ def mrpcsearch(mat1):
 def home(): 
     query = request.args['query']
     print(query)
-    return mrpcsearch(int(query)).replace('"', '')
+    return mrpcsearch(int(query))
 
 def inventorysearch(mat1):
 	df = pd.read_csv("inventorydata.csv")
